e7f80b389679a6f4148b0dee4a1f8476a5a38091
girmatesfaye <girmaglory@gmail.com>
2026-03-01 09:02:03 -0800

    Enhance CSS for layout consistency and responsiveness
    - Added min-height to .app-sidebar and .app-main for full viewport height.
    - Set min-height for .sidebar-brand and .app-topbar to ensure uniformity.
    - Adjusted padding for .app-topbar to remove unnecessary spacing.
    - Removed redundant styles for .app-sidebar in mobile view to streamline layout.
    - Introduced a new compressed file in the build output.

--- obj/Debug/net10.0/TaskManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net10.0/TaskManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+510b455575d3b85d3d057aca486f8bd79b22483a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91314b315e6eeca8e4ed34fe2b892bd39af2d971")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
